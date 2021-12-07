a781d45cfc821f46a9715995444e774cfd9fc5e5
Eltuss <elton.s.barbosa@outlook.com>
2021-12-07 15:28:20 -0300

    Criar Activity empyt Splash

---
+++ app/src/main/java/com/example/webscrapingjsoup/Splash.kt
@@ -0,0 +1,10 @@
+package com.example.webscrapingjsoup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
