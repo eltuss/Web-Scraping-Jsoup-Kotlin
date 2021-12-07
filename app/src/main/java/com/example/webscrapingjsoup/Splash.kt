d9bf350ec91ecd05cc19bc7e8fb5ff999fa5e016
Eltuss <elton.s.barbosa@outlook.com>
2021-12-07 16:44:24 -0300

    Mostrar o resultado da busca com um Log

--- app/src/main/java/com/example/webscrapingjsoup/Splash.kt
+++ app/src/main/java/com/example/webscrapingjsoup/Splash.kt
@@ -3,14 +3,22 @@ package com.example.webscrapingjsoup
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 import org.w3c.dom.Document
 import java.io.IOException
 
 class Splash : AppCompatActivity() {
+
+    private var loader: AsyncTask<Void, Void, ArrayList<New>>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        loader = LoadInitNews()
+        loader!!.execute()
+
     }
 
     internal class LoadInitNews : AsyncTask<Void, Void, ArrayList<New>>(){
@@ -19,13 +27,13 @@ class Splash : AppCompatActivity() {
 
         override fun doInBackground(vararg p0: Void?): ArrayList<New> {
             try {
-                val url = "https://antigo.saude.gov.br/fakenews/?limitstart=0"
+                val url = "https://antigo.saude.gov.br/fakenews/"
 
                 val doc: org.jsoup.nodes.Document = Jsoup.connect(url).get()
                 //Pega a imagem dentro da div
-                val div: Elements = doc.select("div.titleImage")
+                val div: Elements = doc.select("div.tileItem")
                 //Pega o titulo dentro do H2
-                val tagHeading: Elements = doc.select("h2.titleHeadLine")
+                val tagHeading: Elements = doc.select("h2.tileHeadline")
 
                 val size: Int = div.size
                 for (index in 0..size){
@@ -36,12 +44,14 @@ class Splash : AppCompatActivity() {
                     //Pega os detalhes da noticia dentro da tag "a" com o atributo "href"
                     val details: String = tagHeading.select("a").attr("href")
 
-                    news.add(New("https://antigo.saude.gov.br"+imgUrl, title, details))
+                    Log.i("Result", "$imgUrl $title $details")
+                    news.add(New("https://antigo.saude.gov.br$imgUrl", title, details))
                 }
 
             }catch (e: IOException){
                 e.printStackTrace()
             }
+            return  news
         }
 
     }
