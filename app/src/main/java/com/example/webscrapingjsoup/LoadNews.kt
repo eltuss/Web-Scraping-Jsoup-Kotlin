3f84af6c9efd00b7d7ccd8e6b50f76fd41556e0d
Eltuss <elton.s.barbosa@outlook.com>
2021-12-10 10:29:35 -0300

    Vamos Colocar nossa classe AsyncTask em uma classe separada para podermos reutiliza-la no Main tbm

---
+++ app/src/main/java/com/example/webscrapingjsoup/LoadNews.kt
@@ -0,0 +1,45 @@
+package com.example.webscrapingjsoup
+
+import android.os.AsyncTask
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
+import java.io.IOException
+
+class LoadInitNews(var activity: AppCompatActivity?): AsyncTask<Void, Void, ArrayList<New>>(){
+
+        private var news: ArrayList<New> = ArrayList()
+
+        override fun doInBackground(vararg p0: Void?): ArrayList<New> {
+            try {
+                val url = "https://antigo.saude.gov.br/fakenews/"
+
+                val doc: org.jsoup.nodes.Document = Jsoup.connect(url).get()
+                //Pega a imagem dentro da div
+                val div: Elements = doc.select("div.tileItem")
+                //Pega o titulo dentro do H2
+                val tagHeading: Elements = doc.select("h2.tileHeadline")
+
+                val size: Int = div.size
+                for (index in 0..size){
+                    //Pega o link da imagem dentro da tag "img" com o atributo src
+                    val imgUrl: String = div.select("img").eq(index).attr("src")
+                    //Pega o texto dentro da tag "a"
+                    val title: String = tagHeading.select("a").eq(index).text()
+                    //Pega os detalhes da noticia dentro da tag "a" com o atributo "href"
+                    val details: String = tagHeading.select("a").attr("href")
+
+                    Log.i("Result", "$imgUrl $title $details")
+                    news.add(New("https://antigo.saude.gov.br$imgUrl", title, details))
+                }
+
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+            return  news
+        }
+
+        override fun onPostExecute(result: ArrayList<New>?) {}
+
+}
--- app/src/main/java/com/example/webscrapingjsoup/Splash.kt
+++ app/src/main/java/com/example/webscrapingjsoup/Splash.kt
@@ -21,41 +21,7 @@ class Splash : AppCompatActivity() {
 
     }
 
-    //Precisa desse var como parametro para abrir a outra tela lá em baixo
-    internal class LoadInitNews(var activity: AppCompatActivity?): AsyncTask<Void, Void, ArrayList<New>>(){
 
-        private var news: ArrayList<New> = ArrayList()
-
-        override fun doInBackground(vararg p0: Void?): ArrayList<New> {
-            try {
-                val url = "https://antigo.saude.gov.br/fakenews/"
-
-                val doc: org.jsoup.nodes.Document = Jsoup.connect(url).get()
-                //Pega a imagem dentro da div
-                val div: Elements = doc.select("div.tileItem")
-                //Pega o titulo dentro do H2
-                val tagHeading: Elements = doc.select("h2.tileHeadline")
-
-                val size: Int = div.size
-                for (index in 0..size){
-                    //Pega o link da imagem dentro da tag "img" com o atributo src
-                    val imgUrl: String = div.select("img").eq(index).attr("src")
-                    //Pega o texto dentro da tag "a"
-                    val title: String = tagHeading.select("a").eq(index).text()
-                    //Pega os detalhes da noticia dentro da tag "a" com o atributo "href"
-                    val details: String = tagHeading.select("a").attr("href")
-
-                    Log.i("Result", "$imgUrl $title $details")
-                    news.add(New("https://antigo.saude.gov.br$imgUrl", title, details))
-                }
-
-            }catch (e: IOException){
-                e.printStackTrace()
-            }
-            return  news
-        }
-
-        override fun onPostExecute(result: ArrayList<New>?) {
             val intent = Intent(activity, MainActivity::class.java)
             activity!!.startActivity(intent)
             activity!!.finish()
