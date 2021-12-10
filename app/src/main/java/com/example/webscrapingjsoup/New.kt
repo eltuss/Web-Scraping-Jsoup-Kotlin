ed29b0224c70e419bccd49f2368fb69c9ba2f1fd
Eltuss <elton.s.barbosa@outlook.com>
2021-12-10 11:06:48 -0300

    Tornar a classe New Serializable

--- app/src/main/java/com/example/webscrapingjsoup/New.kt
+++ app/src/main/java/com/example/webscrapingjsoup/New.kt
@@ -1,3 +1,5 @@
 package com.example.webscrapingjsoup
 
-class New (var image: String, var title : String, var details: String)
+import java.io.Serializable
+
+class New (var image: String, var title : String, var details: String):Serializable
--- app/src/main/java/com/example/webscrapingjsoup/Splash.kt
+++ app/src/main/java/com/example/webscrapingjsoup/Splash.kt
@@ -30,5 +30,3 @@ class Splash : AppCompatActivity(), IJsoupData {
 
     }
 }
-
-}
