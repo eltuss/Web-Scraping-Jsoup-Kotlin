de774680e012116efa6ed358ea886335e91fb670
Eltuss <elton.s.barbosa@outlook.com>
2021-12-12 10:28:05 -0300

    Classe DetailsNewsActivity foi criada

---
+++ app/src/main/java/com/example/webscrapingjsoup/DetailsNewsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.webscrapingjsoup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsNewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_news)
+    }
+}
