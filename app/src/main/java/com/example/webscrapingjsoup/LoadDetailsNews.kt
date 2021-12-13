de29521069cb84132eafd1a3fcaccaefdcf5214f
Eltuss <elton.s.barbosa@outlook.com>
2021-12-13 15:57:52 -0300

    Criado classe LoadDetailsNews

---
+++ app/src/main/java/com/example/webscrapingjsoup/LoadDetailsNews.kt
@@ -0,0 +1,11 @@
+package com.example.webscrapingjsoup
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+
+class LoadDetailsNews(activity: AppCompatActivity): AsyncTask<Void, Void, ArrayList<String>>() {
+
+    override fun doInBackground(vararg p0: Void?): ArrayList<String> {
+        TODO("Not yet implemented")
+    }
+}
