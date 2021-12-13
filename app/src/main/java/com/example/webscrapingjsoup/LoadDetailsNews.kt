b2ddbf32805fb62bfc90c247d94e448397f61593
Eltuss <elton.s.barbosa@outlook.com>
2021-12-13 16:19:11 -0300

    Filtrar apenas o texto sobre a noticia com Jsoup

--- app/src/main/java/com/example/webscrapingjsoup/LoadDetailsNews.kt
+++ app/src/main/java/com/example/webscrapingjsoup/LoadDetailsNews.kt
@@ -2,10 +2,35 @@ package com.example.webscrapingjsoup
 
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import java.io.IOException
 
-class LoadDetailsNews(activity: AppCompatActivity): AsyncTask<Void, Void, ArrayList<String>>() {
+class LoadDetailsNews(activity: AppCompatActivity, var urlDetails: String):
+    AsyncTask<Void, Void, ArrayList<String>>() {
+
+    private var details: ArrayList<String>? = ArrayList()
 
     override fun doInBackground(vararg p0: Void?): ArrayList<String> {
-        TODO("Not yet implemented")
+        try {
+            val baseUrl = "https://jovemnerd.com.br/nerdbunker/"
+            val url = baseUrl+urlDetails
+
+            val doc: Document = Jsoup.connect(url).get()
+            val div: Elements = doc.select("div.content-left")
+            div.select("div").remove()
+            //Aqui ele pega apenas o texto dentro das tags "p" e "li"
+            val textComplete = div.select("p, ul > li")
+
+            for (element in textComplete){
+                if (element.text() != "")
+                    details!!.add(element.text())
+            }
+
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+        return details!!
     }
 }
