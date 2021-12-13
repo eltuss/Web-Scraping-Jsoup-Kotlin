f020e57faf9e8428d919b32fdf62c4f250f012bd
Eltuss <elton.s.barbosa@outlook.com>
2021-12-13 15:49:43 -0300

    Editado classe DetailsNewsActivity para receber os itens do recyclerView

--- app/src/main/java/com/example/webscrapingjsoup/DetailsNewsActivity.kt
+++ app/src/main/java/com/example/webscrapingjsoup/DetailsNewsActivity.kt
@@ -2,10 +2,23 @@ package com.example.webscrapingjsoup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_details_news.*
 
 class DetailsNewsActivity : AppCompatActivity() {
+
+    var urlImage: String? = null
+    var urlDetails: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details_news)
+
+        urlImage = intent.getStringExtra("IMAGE")
+        urlDetails = intent.getStringExtra("DETAILS")
+
+        txt_title_detail!!.text = intent.getStringExtra("TITLE")
+        Picasso.get().load(urlImage).into(image_detail)
+
     }
 }
