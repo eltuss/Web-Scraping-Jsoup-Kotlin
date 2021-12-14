e15bbc624e248868335526ad9f01ebb13e0f885c
Eltuss <elton.s.barbosa@outlook.com>
2021-12-14 16:25:08 -0300

    Função de compartilhamento criada

--- app/src/main/java/com/example/webscrapingjsoup/DetailsNewsActivity.kt
+++ app/src/main/java/com/example/webscrapingjsoup/DetailsNewsActivity.kt
@@ -1,13 +1,24 @@
 package com.example.webscrapingjsoup
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.content.FileProvider.getUriForFile
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.activity_details_news.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.lang.Exception
 
 class DetailsNewsActivity : AppCompatActivity(), ILoadDetails {
 
@@ -32,6 +43,46 @@ class DetailsNewsActivity : AppCompatActivity(), ILoadDetails {
 
     }
 
+    //Função para compartilhar o item
+    fun shareNewsImage(){
+        Picasso.get().load(urlImage).into(object : Target{
+
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {  }
+
+            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) { }
+
+            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.setType("image/*")
+                intent.putExtra(Intent.EXTRA_STREAM, getLocalBitmapUri(bitmap!!))
+                startActivity(Intent.createChooser(intent, "Share"))
+            }
+
+            private fun getLocalBitmapUri(bitmap: Bitmap): Uri? {
+
+                var bitmapUri: Uri? = null
+                try {
+
+                    val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                    "shareimage" + ".png")
+
+                    val out = FileOutputStream(file)
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, out)
+                    out.close()
+
+                    bitmapUri = getUriForFile(applicationContext,
+                        applicationContext.packageName + ".fileprovider",
+                    file)
+
+                }catch(e : IOException){
+                    e.printStackTrace()
+                }
+                return bitmapUri
+            }
+
+        })
+    }
+
     //função que manipula o menu na barra superior da tela
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_activity_details_news, menu)
@@ -43,6 +94,8 @@ class DetailsNewsActivity : AppCompatActivity(), ILoadDetails {
 
         when(id){
             android.R.id.home -> finish() //Aqui ele volta para a tela de home(principal) MainActivity
+            R.id.btn_share -> shareNewsImage()
+
         }
 
         return super.onOptionsItemSelected(item)
